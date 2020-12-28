27b5e939bbe15be89fdf2df83008fb14deeed1d3
Markus Kirkines <32905837+mkirkines@users.noreply.github.com>
2020-12-28 20:56:26 +0100

    added some blank lines for readability

--- src/space4x/constants.py
+++ src/space4x/constants.py
@@ -1,12 +1,15 @@
 hex_tile_scale = 0.25
 hex_tile_width = 100
 hex_tile_height = 116
+
 hex_grid_margin_x = 4
 hex_grid_margin_y = 4
 hex_grid_correction_x = 2
 hex_grid_correction_y = 21
 hex_grid_dim_x = 20
 hex_grid_dim_y = 20
+
 mouse_img_scale = 0.03
+
 star_img_scale = 0.5
 star_to_hex_ratio = 0.2
